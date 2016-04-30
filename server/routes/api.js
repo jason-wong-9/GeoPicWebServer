6c59d56001469452b1f5a11e347ac0c15dd3bf39
Jason Wong <jason_19960903@hotmail.com>
2016-04-30 10:51:42 -0700

    User authentication working

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,6 +4,18 @@ var config = require('../../config');
 
 var secretKey = config.secretKey;
 
+var jsonwebtoken = require('jsonwebtoken');
+
+function generateToken (user) {
+	var token = jsonwebtoken.sign({
+		id: user._id,
+		username: user.username
+	}, secretKey, {
+		expiresInMinute: 1440
+	});
+	return token;
+}
+
 module.exports = function(app, express){
 	var api = express.Router();
 	api.post('/signup', function(req, res){
@@ -11,6 +23,7 @@ module.exports = function(app, express){
 			username: req.body.username,
 			password: req.body.password
 		});
+		var token = generateToken(user);
 		user.save(function(err){
 			if (err){
 				console.log(err);
@@ -19,12 +32,14 @@ module.exports = function(app, express){
 			}  else {
 				res.json({
 					success: true,
-					message: "User has been created!"
+					message: "User has been created!",
+					token: token
 				});
 			}
 		});
 	});
-	api.post('login', function(req, res){
+
+	api.post('/login', function(req, res){
 		User.findOne({
 			username: req.body.username
 		}).select('username password').exec(function(err, user) {
@@ -38,13 +53,46 @@ module.exports = function(app, express){
 				if (!validPassword){
 					res.send({ message: "Invalid Password "})
 				} else {
+					var token = generateToken(user);
+
 					res.json({
 						success: true,
-						message: "Successfully login!"
+						message: "Successfully login!", 
+						token: token
 					});
 				}
 			}
 		});
 	});
+
+	api.use(function(req, res, next){
+        console.log("Checking for token");
+        var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+        
+        // check if token exist
+        if(token) {
+            jsonwebtoken.verify(token, secretKey, function(err, decoded){
+               if(err) {
+                   res.status(403).send({ success: false, message: "Failed to authenticate!"});
+               } else {
+                   req.decoded = decoded;
+                   next();
+               }
+            });
+        } else {
+            res.status(403).send({ success: false, message: "No Token Provided" });
+        }
+    });
+
+	api.get('/me', function(req, res){
+		res.json(req.decoded);
+	});
+	// api.post('/upload', function(req, res){
+	// 	var sticker = new Sticker({
+	// 		 link: req.body.link,
+
+	// 	});
+	// });
+
 	return api;
 }
